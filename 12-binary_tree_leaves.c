7eeefc575fdd6001519d7daa3132a82db253c5f6
AI-Amponsah <yawamp27@gmail.com>
2023-05-01 19:27:21 +0000

    leaves

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+
+#include "binary_trees.h"
+
+/**
+  *binary_tree_leaves - Counts the number of leaves
+  *@tree: pointer to  tree
+  *
+  *Return: Number of trees
+*/
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+
+
+}
