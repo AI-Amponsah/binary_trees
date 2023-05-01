54fbf47d514919b25440ab943df2e2264749360f
AI-Amponsah <yawamp27@gmail.com>
2023-05-01 14:16:25 +0000

    delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+
+#include "binary_trees.h"
+
+/**
+  *binary_tree_insert_delete - deletes a binary tree
+  *@tree: pointer to the tree
+  *
+  *Return: void
+  */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+
+		free(tree);
+	}
+}
